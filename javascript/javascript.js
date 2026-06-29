2ad9a8a18c769ba4d499a97c973a921da1faa0f8
couououou <bluemoon6690@naver.com>
2026-06-29 15:35:08 +0900

    1

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -618,18 +618,6 @@ window.addEventListener('load', function () {
         }, 300);
     });
 
-
-    window.addEventListener("pageshow", (e) => {
-    console.log("persisted:", e.persisted);
-});
-
-window.addEventListener("pagehide", () => {
-    console.log("pagehide");
-});
-
-window.addEventListener("load", () => {
-    console.log("load");
-});
 });
 
 
